ac6d8de67b26093e7f06ba3bc1f9cf9998021178
bartox7777 <bartosz.zelek@protonmail.com>
2022-12-25 03:29:40 +0100

    simple takes implemented

--- Referee.java
+++ Referee.java
@@ -32,6 +32,14 @@ public class Referee {
         }
     }
 
+    private char getSymbolOfOpponent(){
+        if (currentPlayer==player.WHITE){
+            return '@';
+        } else {
+            return 'O';
+        }
+    }
+
     // move in format "a3 b4" converted to coordinates [0][2] [1][3]
     private int[][] convertMoveToCoordinates(String move){
         int[][] coordinates = new int[2][2];
@@ -42,8 +50,39 @@ public class Referee {
         return coordinates;
     }
 
+    // check if move is valid when user wants to take a pawn
+    // move is in format "a3 b4x c5 [...]"; a3 is the pawn to move, b4x is the pawn to take, c5 is the destination
+    private boolean isMoveWithTakeValid(String move){
+        // TODO: make possibility for more than one take
+        if (move.length()>=9){
+            if (move.charAt(5)=='x'){
+                int[][] coordinates = convertMoveToCoordinates(move.substring(0,5));
+                int x1 = coordinates[0][0];
+                int y1 = coordinates[0][1];
+                int x2 = coordinates[1][0];
+                int y2 = coordinates[1][1];
+                char[][] board = this.board.getBoard();
+                if (board[x1][y1]==getSymbolOfCurrentPlayer() && board[x2][y2]==getSymbolOfOpponent()){
+                    coordinates = convertMoveToCoordinates(move.substring(4)); // get coordinates of destination; only coordinates [1][0] and [1][1] are used
+                    int x3 = coordinates[1][0];
+                    int y3 = coordinates[1][1];
+                    if (board[x3][y3]==' '){
+                        if (Math.abs(x1-x3)==2 && Math.abs(y1-y3)==2){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     public boolean isMoveValid(String move){
+        // TODO: check if move is inside the board
         if (move.length()!=5){
+            if (move.contains("x")){ // user wants to take a pawn(s)
+                return isMoveWithTakeValid(move);
+            }
             return false;
         }
         int[][] coordinates = convertMoveToCoordinates(move);
@@ -64,17 +103,33 @@ public class Referee {
 
     // make a move on the board
     // return new state of the board
-    // move is in format "a2 a3"
+    // move is in format "a3 b4" or "a3 b4x c5 [...]"
     public Board makeMove(String move){
-        int[][] coordinates = convertMoveToCoordinates(move);
-        int x1 = coordinates[0][0];
-        int y1 = coordinates[0][1];
-        int x2 = coordinates[1][0];
-        int y2 = coordinates[1][1];
-        char[][] board = this.board.getBoard();
-        board[x2][y2]=board[x1][y1];
-        board[x1][y1]=' ';
-        this.board.setBoard(board);
+        if (!move.contains("x")){ // move without taking a pawn
+            int[][] coordinates = convertMoveToCoordinates(move);
+            int x1 = coordinates[0][0];
+            int y1 = coordinates[0][1];
+            int x2 = coordinates[1][0];
+            int y2 = coordinates[1][1];
+            char[][] board = this.board.getBoard();
+            board[x2][y2]=board[x1][y1];
+            board[x1][y1]=' ';
+            this.board.setBoard(board);
+        } else{ // move with taking a pawn
+            // TODO: make possibility for more than one take
+            int[][] coordinates = convertMoveToCoordinates(move);
+            int x1 = coordinates[0][0];
+            int y1 = coordinates[0][1];
+            int x2 = coordinates[1][0];
+            int y2 = coordinates[1][1];
+            coordinates = convertMoveToCoordinates(move.substring(4)); // get coordinates of destination; only coordinates [1][0] and [1][1] are used
+            int x3 = coordinates[1][0];
+            int y3 = coordinates[1][1];
+            char[][] board = this.board.getBoard();
+            board[x3][y3]=board[x1][y1];
+            board[x1][y1]=' ';
+            board[x2][y2]=' ';
+        }
         return this.board;
     }
 }
