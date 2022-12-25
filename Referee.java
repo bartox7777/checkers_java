9a7a87bfb5b2b8b13c292a66afcf1daf3d6c0535
bartox7777 <bartosz.zelek@protonmail.com>
2022-12-25 16:05:38 +0100

    more than one take possible

--- Referee.java
+++ Referee.java
@@ -40,7 +40,7 @@ public class Referee {
         }
     }
 
-    // move in format "a3 b4" converted to coordinates [0][2] [1][3]
+    // move ONLY in format "a3 b4" converted to coordinates [0][2] [1][3]
     private int[][] convertMoveToCoordinates(String move){
         int[][] coordinates = new int[2][2];
         coordinates[0][0] = move.charAt(1)-49;
@@ -53,28 +53,49 @@ public class Referee {
     // check if move is valid when user wants to take a pawn
     // move is in format "a3 b4x c5 [...]"; a3 is the pawn to move, b4x is the pawn to take, c5 is the destination
     private boolean isMoveWithTakeValid(String move){
-        // TODO: make possibility for more than one take
-        if (move.length()>=9){
-            if (move.charAt(5)=='x'){
-                int[][] coordinates = convertMoveToCoordinates(move.substring(0,5));
-                int x1 = coordinates[0][0];
-                int y1 = coordinates[0][1];
-                int x2 = coordinates[1][0];
-                int y2 = coordinates[1][1];
-                char[][] board = this.board.getBoard();
-                if (board[x1][y1]==getSymbolOfCurrentPlayer() && board[x2][y2]==getSymbolOfOpponent()){
-                    coordinates = convertMoveToCoordinates(move.substring(4)); // get coordinates of destination; only coordinates [1][0] and [1][1] are used
-                    int x3 = coordinates[1][0];
-                    int y3 = coordinates[1][1];
-                    if (board[x3][y3]==' '){
-                        if (Math.abs(x1-x3)==2 && Math.abs(y1-y3)==2){
-                            return true;
-                        }
-                    }
-                }
+        char [][] boardCopy = new char[8][8];
+        for (int i=0; i<8; i++){
+            for (int j=0; j<8; j++){
+                boardCopy[i][j] = board.getBoard()[i][j];
             }
         }
-        return false;
+
+        // this ifology is bad idea, TODO: refactor
+        Boolean validMove = false;
+        while (true){
+            if (move.length()>=9){
+                    if (move.charAt(5)=='x'){
+                        int[][] coordinates = convertMoveToCoordinates(move.substring(0,5));
+                        int x1 = coordinates[0][0];
+                        int y1 = coordinates[0][1];
+                        int x2 = coordinates[1][0];
+                        int y2 = coordinates[1][1];
+                        // char[][] board = this.board.getBoard();
+                        if (boardCopy[x1][y1]==getSymbolOfCurrentPlayer() && boardCopy[x2][y2]==getSymbolOfOpponent()){
+                            coordinates = convertMoveToCoordinates(move.substring(4)); // get coordinates of destination; only coordinates [1][0] and [1][1] are used
+                            int x3 = coordinates[1][0];
+                            int y3 = coordinates[1][1];
+                            if (boardCopy[x3][y3]==' '){
+                                if (Math.abs(x1-x3)==2 && Math.abs(y1-y3)==2){ //  TODO: take can be done backwards? One take can be done forward and one backward in "one move".
+                                    validMove = true;
+                                    move = move.substring(7); // remove first part of move, eg. "a3 b4x c5" -> "c5"
+                                    boardCopy[x1][y1]=' '; // remove pawn that was moved
+                                    boardCopy[x2][y2]=' '; // remove pawn that was taken
+                                    boardCopy[x3][y3]=getSymbolOfCurrentPlayer(); // move pawn to destination
+                                    System.out.println(move);
+                                    if (!move.contains("x")){
+                                        if (move.length()!=2){
+                                            validMove = false;
+                                        }
+                                        break;
+                                    }
+                                } else {validMove = false; break;}
+                            } else {validMove = false; break;}
+                        } else {validMove = false; break;}
+                    } else {validMove = false; break;}
+            } else {validMove = false; break;}
+        }
+        return validMove;
     }
 
     public boolean isMoveValid(String move){
@@ -116,19 +137,21 @@ public class Referee {
             board[x1][y1]=' ';
             this.board.setBoard(board);
         } else{ // move with taking a pawn
-            // TODO: make possibility for more than one take
-            int[][] coordinates = convertMoveToCoordinates(move);
-            int x1 = coordinates[0][0];
-            int y1 = coordinates[0][1];
-            int x2 = coordinates[1][0];
-            int y2 = coordinates[1][1];
-            coordinates = convertMoveToCoordinates(move.substring(4)); // get coordinates of destination; only coordinates [1][0] and [1][1] are used
-            int x3 = coordinates[1][0];
-            int y3 = coordinates[1][1];
-            char[][] board = this.board.getBoard();
-            board[x3][y3]=board[x1][y1];
-            board[x1][y1]=' ';
-            board[x2][y2]=' ';
+            while (move.length() >= 9){
+                int[][] coordinates = convertMoveToCoordinates(move);
+                int x1 = coordinates[0][0];
+                int y1 = coordinates[0][1];
+                int x2 = coordinates[1][0];
+                int y2 = coordinates[1][1];
+                coordinates = convertMoveToCoordinates(move.substring(4)); // get coordinates of destination; only coordinates [1][0] and [1][1] are used
+                int x3 = coordinates[1][0];
+                int y3 = coordinates[1][1];
+                char[][] board = this.board.getBoard();
+                board[x3][y3]=board[x1][y1];
+                board[x1][y1]=' ';
+                board[x2][y2]=' ';
+                move = move.substring(7); // remove first part of move, eg. "a3 b4x c5" -> "c5"
+            }
         }
         return this.board;
     }
